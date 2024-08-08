786c93d9d3eb5d4bf5e66c0f23bc233d1e7ffe3a
santiesleo <83185169+santiesleo@users.noreply.github.com>
2024-08-08 14:57:34 -0500

    First commit

--- src/main/java/com/example/sprintintro/SprintIntroApplication.java
+++ src/main/java/com/example/sprintintro/SprintIntroApplication.java
@@ -1,6 +1,8 @@
 package com.example.sprintintro;
 
 import com.example.sprintintro.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,18 +12,23 @@ import java.util.logging.Logger;
 
 /*Marca esta clase como la principal clase de configuración de la aplicación Spring Boot*/
 @SpringBootApplication
-public class SprintIntroApplication {
+public class SprintIntroApplication implements CommandLineRunner {
 
 	/*Instancia de "Logger" para registrar mensajes*/
 	private static final Logger log = Logger.getLogger(SprintIntroApplication.class.getName());
 
+	@Autowired
+	/*
+	La anotación @Autowired en Spring se utiliza para realizar la inyección automática de dependencias en un bean.
+	Esto significa que Spring resolverá y suministrará automáticamente los objetos que un bean necesita para funcionar,
+	sin que sea necesario crear manualmente esas instancias.*/
+	private UserRepository userRepository;
+
 	public static void main(String[] args) {
-		/*Carga el contexto de la aplicación desde el xml*/
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		/*Obtiene el bean "UserRepository" del contexto*/
-		UserRepository userRepository = context.getBean(UserRepository.class);
-		/*Registra el número de usuarios en el "UserRepository"*/
-		log.info("The number of users is: " + userRepository.getUsers().size());
+		SpringApplication.run(SprintIntroApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+		log.info("Users in repository: " + userRepository.getUsers().size());
+	}
 }
--- src/main/java/com/example/sprintintro/repositories/UserRepository.java
+++ src/main/java/com/example/sprintintro/repositories/UserRepository.java
@@ -1,11 +1,17 @@
 package com.example.sprintintro.repositories;
 
 import com.example.sprintintro.Model.User;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /*Clase que maneja una lista de usuarios*/
+@Repository
+/*La anotación @Repository en Spring se utiliza para marcar una clase como un componente de acceso a datos (DAO - Data Access Object).
+Esta anotación indica a Spring que la clase es responsable de interactuar con la base de datos o con cualquier otro mecanismo de persistencia,
+y permite que Spring maneje las excepciones específicas de la capa de persistencia.*/
 public class UserRepository {
     private List<User> users;
 
@@ -23,6 +29,14 @@ public class UserRepository {
     }
 
     /*Método de inicialización que agrega un usuario por defecto*/
+    @PostConstruct
+    /*
+    La anotación @PostConstruct en Spring Boot se utiliza para marcar un método que debe ejecutarse una vez que el contenedor de Spring
+    ha terminado de inyectar las dependencias y está a punto de poner el bean en servicio. Es un método de callback que se ejecuta después
+    de que el bean ha sido inicializado, lo que lo hace ideal para cualquier configuración adicional que necesite realizarse después de que
+    el bean ha sido creado y todas las dependencias han sido inyectadas.
+    /*
+     */
     private void init() {
         User user = new User();
         user.setUsername("Jhon Doe");
